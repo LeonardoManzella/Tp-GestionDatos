f37f29f37c18c550eab298e6e22316fb7f9d1a13
Leonardo Manzella <leonardo.manzella@gmail.com>
2016-09-30 10:44:20 -0300

    Agrego 3 Tablas mas

--- creacion-tablas-new.sql
+++ creacion-tablas-new.sql
@@ -32,28 +32,48 @@ http://www.codeproject.com/Articles/1065295/SQL-Server-Table-and-Column-Naming-C
 Si quieren pueden usar esta utilidad online para formatear el codigo: http://www.sqlinform.com/sql_formatter_online.html
 */
 
+CREATE SCHEMA kfc AUTHORIZATION gd GO
 
-CREATE SCHEMA kfc AUTHORIZATION gd
-GO
 CREATE TABLE kfc.USUARIOS
           (
-                    us_id   INT PRIMARY KEY IDENTITY(1,1)
-                  , us_nick VARCHAR(30) UNIQUE NOT NULL
-                  , --No quiero poner valores muy grandes para evitar que pese mucho la base de datos y ande lenta, mejor que ande rapida para nosotros
-                    us_pass       VARCHAR(30) NOT NULL
+                    us_id         INT PRIMARY KEY IDENTITY(1,1)
+                  , us_nick       VARCHAR(30) UNIQUE NOT NULL --No quiero poner valores muy grandes para evitar que pese mucho la base de datos y ande lenta, mejor que ande rapida para nosotros
+                  , us_pass       VARCHAR(30) NOT NULL
                   , us_habilitado BIT NOT NULL
           )
-GO
+          GO
 CREATE TABLE kfc.ROLES
           (
                     rol_id         INT PRIMARY KEY IDENTITY(1,1)
                   , rol_desc       VARCHAR(50) UNIQUE NOT NULL
                   , rol_habilitado BIT NOT NULL
           )
-GO
+          GO
 CREATE TABLE kfc.ROLES_USUARIOS
           (
                     us_id  INT NOT NULL REFERENCES USUARIOS
                   , rol_id INT NOT NULL REFERENCES ROLES
           )
-GO
+          GO
+CREATE TABLE kfc.FUNCIONALIDADES
+          (
+                    func_id   INT PRIMARY KEY IDENTITY(1,1)
+                  , func_desc VARCHAR(50) UNIQUE NOT NULL
+          )
+          GO
+CREATE TABLE kfc.FUNCIONALIDADES_ROLES
+          (
+                    rol_id  INT NOT NULL REFERENCES ROLES
+                  , func_id INT NOT NULL REFERENCES FUNCIONALIDADES
+          )
+          GO
+CREATE TABLE kfc.PLANES
+          (
+                    plan_id                   INT PRIMARY KEY IDENTITY(1,1)
+                  , plan_precio_bono_consulta NUMERIC(18,0) --No existen datos de donde sacar cuotas. No esta en Tabla Original
+                  , plan_precio_bono_farmacia NUMERIC(18,0)
+          )
+          GO
+CREATE TABLE kfc.AFILIADOS
+          (
+          ) GO

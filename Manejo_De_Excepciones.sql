8f2a8a55d1961cd55eff1c47b830573e1453598e
Leonardo Manzella <leonardo.manzella@gmail.com>
2016-10-14 20:51:48 -0300

    BUG Arreglado

--- Manejo_De_Excepciones.sql
+++ Manejo_De_Excepciones.sql
@@ -16,9 +16,9 @@ BEGIN
         BEGIN CATCH
                     PRINT 'Catcheamos Excepciones'
                     IF @@trancount > 0
-                    ROLLBACK TRANSACTION
+                    ROLLBACK TRANSACTION;		-- Importante el PUNTO Y COMA
 
-                    --Importantisimo el PUNTO Y COMA, por favor no se lo olviden
+                    --Importantisimo el proximo PUNTO Y COMA, por favor no se lo olviden
 					-- Si no lo ponen, lo toma como argumento para "ROLLBACK TRANSACTION" y no se ejecutara el THROW, entonces no sirvio de nada
                     ;THROW
         END CATCH

536282dcefb40edb1cd430dcf0647b0216319c4b
raul93 <millonario.93@hotmail.com>
2016-11-05 17:55:28 -0300

    Clase para llenar los combos, alta afiliados

---
+++ ClinicaFrba/ClinicaFrba/Clases/ComboData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicaFrba.Clases
+{
+    public class ComboData
+    {
+        public Int32 identificador { get; set; }
+        public String descripcion { get; set; }
+
+        public ComboData(object id, object desc)
+        {
+            this.descripcion = desc.ToString();
+            this.identificador = Int32.Parse(id.ToString());
+        }
+    }
+}
---
+++ ClinicaFrba/ClinicaFrba/Negocio/ABMAFIL.cs
@@ -0,0 +1,39 @@
+﻿using ClinicaFrba.Clases;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicaFrba.Negocio
+{
+    public static class ABMAFIL
+    { //Acá irían si hubieran validaciones sobre los datos que no sean sobre el  tipo
+
+
+        /// <summary>
+        /// Obtiene los datos necesarios para el alta del nuevo afiliado
+        /// </summary>
+        /// <param name="afil"></param>
+        public static void alta_afiliado(Afiliado afil)
+        {
+            Base_de_Datos.InteraccionDB.alta_afiliado(afil);
+        }
+
+        public static List<ComboData> get_Estados_Civiles()
+        {
+            return Base_de_Datos.InteraccionDB.get_estados_civiles();
+        }
+
+        public static List<ComboData> get_Planes_Sociales()
+        {
+            return Base_de_Datos.InteraccionDB.get_planes_sociales();
+        }
+
+        public static List<ComboData> get_Tipos_Documentos()
+        {
+            return Base_de_Datos.InteraccionDB.get_cmb_tipos_documentos();
+        }
+    }
+}

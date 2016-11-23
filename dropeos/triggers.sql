2115db0614f9f47247eef6b9789916f9016e6b1e
Leonardo Manzella <leonardo.manzella@gmail.com>
2016-11-22 23:37:22 -0300

    Arreglo Triggers

---
+++ dropeos/triggers.sql
@@ -0,0 +1,27 @@
+IF OBJECT_ID('KFC.existe_afiliado_principal') IS NOT NULL
+DROP TRIGGER KFC.existe_afiliado_principal;
+GO
+
+IF OBJECT_ID('KFC.rol_nueva_funcionalidad') IS NOT NULL
+DROP TRIGGER KFC.Drol_nueva_funcionalidadDD;
+GO
+
+IF OBJECT_ID('KFC.existe_rol_habilitar_deshabilitar') IS NOT NULL
+DROP TRIGGER KFC.existe_rol_habilitar_deshabilitar;
+GO
+
+IF OBJECT_ID('KFC.nueva_agenda_profesional') IS NOT NULL
+DROP TRIGGER KFC.nueva_agenda_profesional;
+GO
+
+IF OBJECT_ID('KFC.impresion_bono') IS NOT NULL
+DROP TRIGGER KFC.impresion_bono;
+GO
+
+IF OBJECT_ID('KFC.nuevo_turno') IS NOT NULL
+DROP TRIGGER KFC.nuevo_turno;
+GO
+
+IF OBJECT_ID('KFC.nuevo_bono') IS NOT NULL
+DROP TRIGGER KFC.nuevo_bono;
+GO

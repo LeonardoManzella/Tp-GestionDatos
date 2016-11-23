650bde593ae4e1aa624adca7bbb84e9e312a26c3
gonza-utn <gonzalo2014@gmail.com>
2016-11-22 21:39:59 -0300

    prueba
    prueba guini-leo

--- ClinicaFrba/ClinicaFrba/Cancelar Turnos/CancelarTurnoProfesional.Designer.cs
+++ ClinicaFrba/ClinicaFrba/Cancelar Turnos/CancelarTurnoProfesional.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(103, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "prueba Guini";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // CancelarTurnoProfesional
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
             this.Name = "CancelarTurnoProfesional";
             this.Text = "CancelarTurnoProfesional";
             this.Load += new System.EventHandler(this.CancelarTurnoProfesional_Load);
@@ -43,5 +54,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }

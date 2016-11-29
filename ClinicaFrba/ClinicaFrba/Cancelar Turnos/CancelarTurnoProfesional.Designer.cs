6c929f77ee9f1ad42027c22513e66eefbe1e656b
vahanishe <vahanishe@users.noreply.github.com>
2016-11-29 16:37:49 -0300

    Se agrega el diseño

--- ClinicaFrba/ClinicaFrba/Cancelar Turnos/CancelarTurnoProfesional.Designer.cs
+++ ClinicaFrba/ClinicaFrba/Cancelar Turnos/CancelarTurnoProfesional.Designer.cs
@@ -28,139 +28,109 @@ namespace ClinicaFrba.CancelarTurno
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.fechaDesdePicker = new System.Windows.Forms.DateTimePicker();
+            this.cancelarTurnoProButton = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fechaHastaPicker = new System.Windows.Forms.DateTimePicker();
+            this.motivoTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // fechaDesdePicker
             // 
-            this.groupBox1.Controls.Add(this.radioButton2);
-            this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(39, 30);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(369, 68);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Tipo de cancelacion";
+            this.fechaDesdePicker.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fechaDesdePicker.Location = new System.Drawing.Point(42, 68);
+            this.fechaDesdePicker.MinDate = new System.DateTime(1970, 11, 29, 0, 0, 0, 0);
+            this.fechaDesdePicker.Name = "fechaDesdePicker";
+            this.fechaDesdePicker.Size = new System.Drawing.Size(369, 20);
+            this.fechaDesdePicker.TabIndex = 1;
+            this.fechaDesdePicker.Value = new System.DateTime(2016, 11, 29, 0, 0, 0, 0);
+            this.fechaDesdePicker.ValueChanged += new System.EventHandler(this.fechaDesdePicker_ValueChanged);
             // 
-            // radioButton2
+            // cancelarTurnoProButton
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(192, 28);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(171, 28);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Rango de fechas";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Checked = true;
-            this.radioButton1.Location = new System.Drawing.Point(30, 28);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(160, 28);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Turno particular";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker1.Location = new System.Drawing.Point(39, 140);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(369, 20);
-            this.dateTimePicker1.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(35, 113);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(180, 24);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Seleccione la fecha:";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(39, 199);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(369, 21);
-            this.comboBox1.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(35, 172);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(178, 24);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Seleccione el turno:";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(148, 243);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(140, 43);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Cancelar turno/os";
-            this.button1.UseVisualStyleBackColor = true;
+            this.cancelarTurnoProButton.Location = new System.Drawing.Point(149, 247);
+            this.cancelarTurnoProButton.Name = "cancelarTurnoProButton";
+            this.cancelarTurnoProButton.Size = new System.Drawing.Size(140, 43);
+            this.cancelarTurnoProButton.TabIndex = 5;
+            this.cancelarTurnoProButton.Text = "Cancelar turnos";
+            this.cancelarTurnoProButton.UseVisualStyleBackColor = true;
+            this.cancelarTurnoProButton.Click += new System.EventHandler(this.cancelarTurnoProButton_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(148, 292);
+            this.button2.Location = new System.Drawing.Point(149, 296);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(140, 23);
             this.button2.TabIndex = 6;
             this.button2.Text = "Cancelar";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Seleccione la(s) fecha(s):";
+            // 
+            // fechaHastaPicker
+            // 
+            this.fechaHastaPicker.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fechaHastaPicker.Location = new System.Drawing.Point(42, 94);
+            this.fechaHastaPicker.Name = "fechaHastaPicker";
+            this.fechaHastaPicker.Size = new System.Drawing.Size(369, 20);
+            this.fechaHastaPicker.TabIndex = 8;
+            // 
+            // motivoTextBox
+            // 
+            this.motivoTextBox.Location = new System.Drawing.Point(42, 151);
+            this.motivoTextBox.Multiline = true;
+            this.motivoTextBox.Name = "motivoTextBox";
+            this.motivoTextBox.Size = new System.Drawing.Size(369, 78);
+            this.motivoTextBox.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 24);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Especifique el motivo:";
             // 
             // CancelarTurnoProfesional
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(455, 347);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(455, 335);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.motivoTextBox);
+            this.Controls.Add(this.fechaHastaPicker);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.cancelarTurnoProButton);
+            this.Controls.Add(this.fechaDesdePicker);
             this.Name = "CancelarTurnoProfesional";
             this.Text = " ";
             this.Load += new System.EventHandler(this.CancelarTurnoProfesional_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker fechaDesdePicker;
+        private System.Windows.Forms.Button cancelarTurnoProButton;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker fechaHastaPicker;
+        private System.Windows.Forms.TextBox motivoTextBox;
+        private System.Windows.Forms.Label label1;
     }
 }

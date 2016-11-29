3f00c64c3a0a5c47225b26b653476fde11833335
vahanishe <vahanishe@users.noreply.github.com>
2016-11-29 16:35:53 -0300

    Avance de Cancelar Turno Profesional

--- ClinicaFrba/ClinicaFrba/Cancelar Turnos/CancelarTurnoProfesional.cs
+++ ClinicaFrba/ClinicaFrba/Cancelar Turnos/CancelarTurnoProfesional.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +12,8 @@ namespace ClinicaFrba.CancelarTurno
 {
     public partial class CancelarTurnoProfesional : Form
     {
+        public Usuario usuario { get; set; }
+
         public CancelarTurnoProfesional()
         {
             InitializeComponent();
@@ -19,9 +21,42 @@ namespace ClinicaFrba.CancelarTurno
 
         private void CancelarTurnoProfesional_Load(object sender, EventArgs e)
         {
-
+            fechaDesdePicker.MinDate = DateTime.Today.AddDays(1);
+            fechaHastaPicker.MinDate = DateTime.Today.AddDays(1);
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void fechaDesdePicker_ValueChanged(object sender, EventArgs e)
+        {
+            fechaHastaPicker.MinDate = fechaDesdePicker.Value;
+        }
+
+        private void cancelarTurnoProButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DateTime fechaDesde = fechaDesdePicker.Value;
+                DateTime fechaHasta = fechaHastaPicker.Value;
+                string motivo = motivoTextBox.Text.ToString();
+
+                if (motivo.Trim().Equals(""))
+                {
+                    MessageBox.Show("Debe completar todos los campos del formulario", "Cancelar Turno", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                Base_de_Datos.BD_Turnos.cancelar_turnos_pro(fechaDesde, fechaHasta, motivo, usuario.id);
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+        }
     }
 
 }

cd8918b7d32fe9c8590bedf21541b86288dec165
Leonardo Manzella <leonardo.manzella@gmail.com>
2016-12-04 12:20:34 -0300

    Uso Datagrid en Comprar Bono

--- ClinicaFrba/ClinicaFrba/Compra Bono/CompraBono.Designer.cs
+++ ClinicaFrba/ClinicaFrba/Compra Bono/CompraBono.Designer.cs
@@ -44,12 +44,19 @@
             this.label_Id_Usuario = new System.Windows.Forms.Label();
             this.textBox_Documento = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.groupBox_filtros = new System.Windows.Forms.GroupBox();
+            this.button_limpiar = new System.Windows.Forms.Button();
+            this.groupBox_plan = new System.Windows.Forms.GroupBox();
+            this.dataGridView_resultados_filtros = new System.Windows.Forms.DataGridView();
+            this.groupBox_filtros.SuspendLayout();
+            this.groupBox_plan.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_resultados_filtros)).BeginInit();
             this.SuspendLayout();
             // 
             // label_plan
             // 
             this.label_plan.AutoSize = true;
-            this.label_plan.Location = new System.Drawing.Point(279, 26);
+            this.label_plan.Location = new System.Drawing.Point(6, 25);
             this.label_plan.Name = "label_plan";
             this.label_plan.Size = new System.Drawing.Size(68, 13);
             this.label_plan.TabIndex = 0;
@@ -58,14 +65,14 @@
             // textBox_Plan
             // 
             this.textBox_Plan.Enabled = false;
-            this.textBox_Plan.Location = new System.Drawing.Point(354, 23);
+            this.textBox_Plan.Location = new System.Drawing.Point(88, 22);
             this.textBox_Plan.Name = "textBox_Plan";
             this.textBox_Plan.Size = new System.Drawing.Size(177, 20);
             this.textBox_Plan.TabIndex = 1;
             // 
             // textBox_Cantidad
             // 
-            this.textBox_Cantidad.Location = new System.Drawing.Point(354, 102);
+            this.textBox_Cantidad.Location = new System.Drawing.Point(88, 87);
             this.textBox_Cantidad.Name = "textBox_Cantidad";
             this.textBox_Cantidad.Size = new System.Drawing.Size(177, 20);
             this.textBox_Cantidad.TabIndex = 11;
@@ -73,7 +80,7 @@
             // label_Cantidad
             // 
             this.label_Cantidad.AutoSize = true;
-            this.label_Cantidad.Location = new System.Drawing.Point(298, 102);
+            this.label_Cantidad.Location = new System.Drawing.Point(25, 87);
             this.label_Cantidad.Name = "label_Cantidad";
             this.label_Cantidad.Size = new System.Drawing.Size(49, 13);
             this.label_Cantidad.TabIndex = 2;
@@ -81,9 +88,10 @@
             // 
             // button_Comprar
             // 
-            this.button_Comprar.Location = new System.Drawing.Point(455, 140);
+            this.button_Comprar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Comprar.Location = new System.Drawing.Point(663, 425);
             this.button_Comprar.Name = "button_Comprar";
-            this.button_Comprar.Size = new System.Drawing.Size(76, 63);
+            this.button_Comprar.Size = new System.Drawing.Size(81, 45);
             this.button_Comprar.TabIndex = 12;
             this.button_Comprar.Text = "Comprar Bonos";
             this.button_Comprar.UseVisualStyleBackColor = true;
@@ -92,7 +100,7 @@
             // label_Usuario
             // 
             this.label_Usuario.AutoSize = true;
-            this.label_Usuario.Location = new System.Drawing.Point(34, 29);
+            this.label_Usuario.Location = new System.Drawing.Point(9, 27);
             this.label_Usuario.Name = "label_Usuario";
             this.label_Usuario.Size = new System.Drawing.Size(81, 13);
             this.label_Usuario.TabIndex = 5;
@@ -100,14 +108,14 @@
             // 
             // textBox_Nombre
             // 
-            this.textBox_Nombre.Location = new System.Drawing.Point(123, 26);
+            this.textBox_Nombre.Location = new System.Drawing.Point(96, 23);
             this.textBox_Nombre.Name = "textBox_Nombre";
             this.textBox_Nombre.Size = new System.Drawing.Size(100, 20);
             this.textBox_Nombre.TabIndex = 6;
             // 
             // textBox_Apellido
             // 
-            this.textBox_Apellido.Location = new System.Drawing.Point(123, 66);
+            this.textBox_Apellido.Location = new System.Drawing.Point(299, 23);
             this.textBox_Apellido.Name = "textBox_Apellido";
             this.textBox_Apellido.Size = new System.Drawing.Size(100, 20);
             this.textBox_Apellido.TabIndex = 8;
@@ -115,7 +123,7 @@
             // label_apellido
             // 
             this.label_apellido.AutoSize = true;
-            this.label_apellido.Location = new System.Drawing.Point(34, 69);
+            this.label_apellido.Location = new System.Drawing.Point(212, 26);
             this.label_apellido.Name = "label_apellido";
             this.label_apellido.Size = new System.Drawing.Size(81, 13);
             this.label_apellido.TabIndex = 7;
@@ -123,18 +131,19 @@
             // 
             // button_Buscar
             // 
-            this.button_Buscar.Location = new System.Drawing.Point(163, 140);
+            this.button_Buscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Buscar.Location = new System.Drawing.Point(625, 17);
             this.button_Buscar.Name = "button_Buscar";
-            this.button_Buscar.Size = new System.Drawing.Size(105, 63);
+            this.button_Buscar.Size = new System.Drawing.Size(96, 55);
             this.button_Buscar.TabIndex = 10;
-            this.button_Buscar.Text = "Buscar Usuario";
+            this.button_Buscar.Text = "Buscar Afiliado";
             this.button_Buscar.UseVisualStyleBackColor = true;
             this.button_Buscar.Click += new System.EventHandler(this.button_Buscar_Click);
             // 
             // textBox_precio
             // 
             this.textBox_precio.Enabled = false;
-            this.textBox_precio.Location = new System.Drawing.Point(354, 55);
+            this.textBox_precio.Location = new System.Drawing.Point(88, 53);
             this.textBox_precio.Name = "textBox_precio";
             this.textBox_precio.Size = new System.Drawing.Size(177, 20);
             this.textBox_precio.TabIndex = 11;
@@ -142,7 +151,7 @@
             // label_precio
             // 
             this.label_precio.AutoSize = true;
-            this.label_precio.Location = new System.Drawing.Point(279, 58);
+            this.label_precio.Location = new System.Drawing.Point(13, 56);
             this.label_precio.Name = "label_precio";
             this.label_precio.Size = new System.Drawing.Size(61, 13);
             this.label_precio.TabIndex = 10;
@@ -150,10 +159,10 @@
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(12, 140);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(316, 424);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(93, 63);
+            this.button1.Size = new System.Drawing.Size(95, 45);
             this.button1.TabIndex = 13;
             this.button1.Text = "Cerrar";
             this.button1.UseVisualStyleBackColor = true;
@@ -163,14 +172,14 @@
             // 
             this.label_Id_Usuario.AutoSize = true;
             this.label_Id_Usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_Id_Usuario.Location = new System.Drawing.Point(28, 11);
+            this.label_Id_Usuario.Location = new System.Drawing.Point(25, 22);
             this.label_Id_Usuario.Name = "label_Id_Usuario";
             this.label_Id_Usuario.Size = new System.Drawing.Size(0, 18);
             this.label_Id_Usuario.TabIndex = 13;
             // 
             // textBox_Documento
             // 
-            this.textBox_Documento.Location = new System.Drawing.Point(123, 102);
+            this.textBox_Documento.Location = new System.Drawing.Point(496, 23);
             this.textBox_Documento.Name = "textBox_Documento";
             this.textBox_Documento.Size = new System.Drawing.Size(100, 20);
             this.textBox_Documento.TabIndex = 9;
@@ -178,38 +187,84 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(34, 105);
+            this.label1.Location = new System.Drawing.Point(428, 27);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(62, 13);
             this.label1.TabIndex = 13;
             this.label1.Text = "Documento";
             // 
+            // groupBox_filtros
+            // 
+            this.groupBox_filtros.Controls.Add(this.label_Usuario);
+            this.groupBox_filtros.Controls.Add(this.label_Id_Usuario);
+            this.groupBox_filtros.Controls.Add(this.textBox_Nombre);
+            this.groupBox_filtros.Controls.Add(this.textBox_Documento);
+            this.groupBox_filtros.Controls.Add(this.label_apellido);
+            this.groupBox_filtros.Controls.Add(this.label1);
+            this.groupBox_filtros.Controls.Add(this.textBox_Apellido);
+            this.groupBox_filtros.Location = new System.Drawing.Point(12, 12);
+            this.groupBox_filtros.Name = "groupBox_filtros";
+            this.groupBox_filtros.Size = new System.Drawing.Size(607, 60);
+            this.groupBox_filtros.TabIndex = 14;
+            this.groupBox_filtros.TabStop = false;
+            this.groupBox_filtros.Text = "Filtros de Busqueda";
+            // 
+            // button_limpiar
+            // 
+            this.button_limpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_limpiar.Location = new System.Drawing.Point(12, 425);
+            this.button_limpiar.Name = "button_limpiar";
+            this.button_limpiar.Size = new System.Drawing.Size(90, 45);
+            this.button_limpiar.TabIndex = 15;
+            this.button_limpiar.Text = "Limpiar Filtros";
+            this.button_limpiar.UseVisualStyleBackColor = true;
+            this.button_limpiar.Click += new System.EventHandler(this.button_limpiar_Click);
+            // 
+            // groupBox_plan
+            // 
+            this.groupBox_plan.Controls.Add(this.label_plan);
+            this.groupBox_plan.Controls.Add(this.textBox_Plan);
+            this.groupBox_plan.Controls.Add(this.label_Cantidad);
+            this.groupBox_plan.Controls.Add(this.textBox_Cantidad);
+            this.groupBox_plan.Controls.Add(this.textBox_precio);
+            this.groupBox_plan.Controls.Add(this.label_precio);
+            this.groupBox_plan.Location = new System.Drawing.Point(12, 295);
+            this.groupBox_plan.Name = "groupBox_plan";
+            this.groupBox_plan.Size = new System.Drawing.Size(277, 117);
+            this.groupBox_plan.TabIndex = 16;
+            this.groupBox_plan.TabStop = false;
+            this.groupBox_plan.Text = "Plan Seleccionado";
+            // 
+            // dataGridView_resultados_filtros
+            // 
+            this.dataGridView_resultados_filtros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_resultados_filtros.Location = new System.Drawing.Point(13, 108);
+            this.dataGridView_resultados_filtros.Name = "dataGridView_resultados_filtros";
+            this.dataGridView_resultados_filtros.Size = new System.Drawing.Size(740, 181);
+            this.dataGridView_resultados_filtros.TabIndex = 17;
+            this.dataGridView_resultados_filtros.CellClick += dataGridView_resultados_filtros_CellClick;
+            // 
             // CompraBono
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(550, 220);
-            this.Controls.Add(this.label_Id_Usuario);
-            this.Controls.Add(this.textBox_Documento);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(765, 476);
+            this.Controls.Add(this.dataGridView_resultados_filtros);
+            this.Controls.Add(this.groupBox_plan);
+            this.Controls.Add(this.button_limpiar);
+            this.Controls.Add(this.groupBox_filtros);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox_precio);
-            this.Controls.Add(this.label_precio);
             this.Controls.Add(this.button_Buscar);
-            this.Controls.Add(this.textBox_Apellido);
-            this.Controls.Add(this.label_apellido);
-            this.Controls.Add(this.textBox_Nombre);
-            this.Controls.Add(this.label_Usuario);
             this.Controls.Add(this.button_Comprar);
-            this.Controls.Add(this.textBox_Cantidad);
-            this.Controls.Add(this.label_Cantidad);
-            this.Controls.Add(this.textBox_Plan);
-            this.Controls.Add(this.label_plan);
             this.Name = "CompraBono";
             this.Text = "Comprar Bonos";
             this.Load += new System.EventHandler(this.CompraBono_Load);
+            this.groupBox_filtros.ResumeLayout(false);
+            this.groupBox_filtros.PerformLayout();
+            this.groupBox_plan.ResumeLayout(false);
+            this.groupBox_plan.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_resultados_filtros)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -231,5 +286,9 @@
         private System.Windows.Forms.Label label_Id_Usuario;
         private System.Windows.Forms.TextBox textBox_Documento;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox_filtros;
+        private System.Windows.Forms.Button button_limpiar;
+        private System.Windows.Forms.GroupBox groupBox_plan;
+        private System.Windows.Forms.DataGridView dataGridView_resultados_filtros;
     }
 }

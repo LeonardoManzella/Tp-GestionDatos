cc2374355ddbf0d6d0fcbcb254d9b97757f94bcc
Leonardo Manzella <leonardo.manzella@gmail.com>
2016-12-06 01:32:12 -0300

    Cambie Lugar BAT Dropeos

--- data/Ejecutar_Scripts_Borrar_Todo.bat
+++
@@ -1,1 +0,0 @@
-sqlcmd -S localhost\SQLSERVER2012 -U gd -P gd2016 -i            dropeos/triggers.sql      ,dropeos/funciones-y-procedures.sql               ,dropeos/drop-tables-esquema.sql  -a 32767 -o resultado_output.txt
